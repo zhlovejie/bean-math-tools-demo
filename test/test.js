88d82a13b8eb4fd27c71182a420343090d55aa7d
zhlovejie <bean_jie@aol.com>
2019-06-17 14:01:56 +0800

    add test cases

--- test/test.js
+++ test/test.js
@@ -2,9 +2,12 @@ const math_tools = require('../src/math-tools.js')
 
 const assert = require('assert')
 
-const add_result = math_tools.add(1,"1")
+assert.strictEqual(2,math_tools.add(1,1))
 
-assert.equal(2,add_result,`2 == ${add_result} fail....`)
-assert.strictEqual(2,add_result,`2 == ${add_result} fail....`)
+assert.strictEqual(0,math_tools.minus(1,1))
+
+assert.strictEqual(4,math_tools.mul(2,2))
+
+assert.strictEqual(1,math_tools.div(1,1))
 
 
