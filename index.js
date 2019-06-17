1a5b1ef59948bd4b832c1c89921a154ed65aa142
zhlovejie <bean_jie@aol.com>
2019-06-17 13:58:09 +0800

    add  methods   mul,div

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const mathTools = require('./src/math-tools.js')
 
 module.exports = {
 	add:mathTools.add,
-	minus:mathTools.minus
+	minus:mathTools.minus,
+	mul:mathTools.mul,
+	div:mathTools.div
 }
--- src/math-tools.js
+++ src/math-tools.js
@@ -6,7 +6,17 @@ function minus(n1,n2){
 	return n1 - n2
 }
 
+function mul(n1,n2){
+	return n1 * n2
+}
+
+function div(n1,n2){
+	return n1 / n2
+}
+
 module.exports = {
 	add:add,
-	minus:minus
+	minus:minus,
+	mul:mul,
+	div:div
 }
